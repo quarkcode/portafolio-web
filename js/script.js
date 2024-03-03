e5acf3b9e7458bdc2a383de1a8bbed28dd2b01e6
quarkcode <miguel@quarkcode.com>
2024-03-03 12:58:40 +0100

    Eliminar todos los archivos

--- js/script.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("load",function(){var e,t=document.querySelector(".pre-loading");(e=t).style.transition="opacity 1.5s",e.style.opacity=0,setTimeout(function(){e.remove()},1500)});
